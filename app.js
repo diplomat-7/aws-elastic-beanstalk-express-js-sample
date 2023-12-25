d6c44bb95265510d852a52293f22e89cfa4b775c
diplomat-7 <nsikan0000@gmail.com>
2023-12-25 12:29:58 +0100

    W

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-
+app.get('/Test', (req, res) => res.send('Hello Test!'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
